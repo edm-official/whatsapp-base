20020d19b61e198efedc66ce58348865bbe34c1e
Endgame Data Marfia <100762985+edm-official@users.noreply.github.com>
2022-09-30 13:18:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -70,17 +70,36 @@ const connectToWA = () => {
 			
 			switch (command) {
 
-case 'alive' : 
-alive(conn ,mek ) 
+case 'alive' : {
+	
+  const templateButtons = [
+  { urlButton: {displayText: 'Youtube' , url: '' }},
+  { urlButton: {displayText: 'Github' , url: '' }},
+  { quickReplyButton: {displayText: 'MENU', id: prefix +'menu' }} , 
+  { quickReplyButton: {displayText: 'OWNER', id: prefix +'owner' }}   
+ ]
+   const buttonMessage = {
+    caption: config.ALIVE_MSG ,
+    footer: config.FOOTER,
+    templateButtons: templateButtons,
+    image: {url: config.ALIVE_LOGO}
+}                                                 
+await conn.sendMessage(from, buttonMessage )
+  
+  }
 break
 
-case 'owner' : 
-own(conn ,mek ) 
+case 'owner' : {
+const vcard = 'BEGIN:VCARD\n' 
+            + 'VERSION:3.0\n' 
+            + `FN:` + config.OWNER_NAME + `\n`
+            + 'TEL;type=CELL;type=VOICE;waid=' + config.OWNER_NUMBER + ':+' + config.OWNER_NUMBER + '\n' 
+            + 'END:VCARD'
+ await conn.sendMessage(from,{ contacts: { displayName: config.OWNER_NAME , contacts: [{ vcard }]  }} , { quoted: mek })
+  }
 break
 
-case 'yt' : 
-play(conn ,mek ,q ) 
-break
+
 				
 				default:
 					
