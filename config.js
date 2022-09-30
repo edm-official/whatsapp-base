f5c7ada1538a9dffa26ad5552cd4cafebf49e911
Endgame Data Marfia <100762985+edm-official@users.noreply.github.com>
2022-09-30 09:17:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.ALIVE_LOGO = process.env.ALIVE_LOGO || 'https://i.ibb.co/SXgrRRx/dd40818b8613.jpg'
-global.ALIVE_MSG = process.env.ALIVE_MSG || 'I am alive'
+config.ALIVE_LOGO = process.env.ALIVE_LOGO || 'https://i.ibb.co/SXgrRRx/dd40818b8613.jpg'
+config.ALIVE_MSG = process.env.ALIVE_MSG || 'I am alive'
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
