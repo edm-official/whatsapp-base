9aaca1ccd3452ec7971f5fd1dd8f214e169e16c5
Endgame Data Marfia <100762985+edm-official@users.noreply.github.com>
2022-09-30 09:05:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 module.exports = {
    
     ALIVEMSG: process.env.ALIVE_MESSAGE === undefined ? 'default' : process.env.ALIVE_MESSAGE,
-    ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.ibb.co/SXgrRRx/dd40818b*613.jpg' : process.env.ALIVE_LOGO,
+    ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.ibb.co/SXgrRRx/dd40818b8613.jpg' : process.env.ALIVE_LOGO,
     
 };
 
