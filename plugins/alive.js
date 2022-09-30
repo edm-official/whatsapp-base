a6763352d95d60fa7de1d9b7b8f3e0eaa8b6b956
Endgame Data Marfia <100762985+edm-official@users.noreply.github.com>
2022-09-30 11:26:26 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,1 +1,22 @@
-l
+const config = require('../config');
+const prefix = '.'
+
+
+async function alive(conn , mek) {
+  const from = mek.key.remoteJid	  
+  const templateButtons = [
+
+  {index: 1, quickReplyButton: {displayText: 'MENU', id: prefix +'menu' }} , 
+  {index: 2, quickReplyButton: {displayText: 'OWNER', id: prefix +'owner' }}   
+ ]
+   const buttonMessage = {
+    caption: config.ALIVE_MSG ,
+    footer: 'EDM BOT BASE',
+    templateButtons: templateButtons,
+    image: {url: config.ALIVE_LOGO}
+}                             
+await conn.sendMessage(from, buttonMessage )
+  }
+}
+
+module.exports =  alive ;
