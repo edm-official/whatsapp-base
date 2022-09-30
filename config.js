a7c0cdd6b7559ac45cd1a5480a0f5a8cddfd1371
Endgame Data Marfia <100762985+edm-official@users.noreply.github.com>
2022-09-30 11:53:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 	ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'Alive Now' : process.env.ALIVE_MSG,
 	ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://img.phonandroid.com/2019/05/whatsapp-publicit%C3%A9s-2020.jpg' : process.env.ALIVE_LOGO,
+        FOOTER: process.env.FOOTER === undefined ? 'EDM BOT BASE' : process.env.FOOTER,
+	OWNER_NAME: process.env.OWNER_NAME === undefined ? 'BUDDHIKA' : process.env.OWNER_NAME,
 };
