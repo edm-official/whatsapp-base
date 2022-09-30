c5f1a577e366aa2957798160f286d1e954888dee
Endgame Data Marfia <100762985+edm-official@users.noreply.github.com>
2022-09-30 12:10:51 +0530

    Create owner.js

---
+++ plugins/owner.js
@@ -0,0 +1,17 @@
+const config = require('../config');
+const prefix = '.'
+async function own(conn , mek ) {
+  const from = mek.key.remoteJid
+  
+ 
+     const vcard = 'BEGIN:VCARD\n' 
+            + 'VERSION:3.0\n' 
+            + `FN:` + config.OWNER_NAME + `\n`
+            + 'TEL;type=CELL;type=VOICE;waid=' + config.OWNER_NUMBER + ':+' + config.OWNER_NUMBER + '\n' 
+            + 'END:VCARD'
+ await conn.sendMessage(from,{ contacts: { displayName: config.OWNER_NAME , contacts: [{ vcard }]  }} , { quoted: mek })
+  }
+  
+  
+
+module.exports = own ;
