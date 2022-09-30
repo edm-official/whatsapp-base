0576902ad99c8c9ee57275daf22bbd2b72f31377
Endgame Data Marfia <100762985+edm-official@users.noreply.github.com>
2022-09-30 12:06:32 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -4,7 +4,6 @@ const prefix = '.'
 
 async function alive(conn , mek) {
   const from = mek.key.remoteJid	
-  var alivemsg = 'Hi ' + pushname '\n\n' + config.ALIVE_MSG
   const buttons = [
 {buttonId: prefix +'menu ', buttonText: {displayText: 'MENU'}, type: 1},
 {buttonId: prefix +'owner ', buttonText: {displayText: 'OWNER'}, type: 1}
