533602eeeba0986e9647172fa94ade554c539d94
Endgame Data Marfia <100762985+edm-official@users.noreply.github.com>
2022-09-30 09:24:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,9 @@
-const fs = require('fs')
-const chalk = require('chalk')
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
-
-config.ALIVE_LOGO = process.env.ALIVE_LOGO || 'https://i.ibb.co/SXgrRRx/dd40818b8613.jpg'
-config.ALIVE_MSG = process.env.ALIVE_MSG || 'I am alive'
-
-let file = require.resolve(__filename)
-fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(chalk.redBright(`Update'${__filename}'`))
-	delete require.cache[file]
-	require(file)
-})
+module.exports = {
+    ALIVEMSG: process.env.ALIVE_MESSAGE === undefined ? 'Alive' : process.env.ALIVE_MESSAGE,
+    ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.ibb.co/SXgrRRx/dd40818b8613.jpg' : process.env.ALIVE_LOGO,
+   
+};
