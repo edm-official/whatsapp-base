6845f86aad00767e03de923610a6fcf56606a019
Endgame Data Marfia <100762985+edm-official@users.noreply.github.com>
2022-09-30 11:17:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,12 @@
-const fs = require('fs')
-const chalk = require('chalk')
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 
-//settings
-global.ALIVE_MSG = process.env.ALIVE_MSG || 'Alive Now'
-global.ALIVE_LOGO = process.env.ALIVE_LOGO || 'https://img.phonandroid.com/2019/05/whatsapp-publicit%C3%A9s-2020.jpg'
-
-let file = require.resolve(__filename)
-fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(chalk.redBright(`Update'${__filename}'`))
-	delete require.cache[file]
-	require(file)
-})
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+	ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'Alive Now' : process.env.ALIVE_MSG,
+	ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://img.phonandroid.com/2019/05/whatsapp-publicit%C3%A9s-2020.jpg' : process.env.ALIVE_LOGO,
+};
